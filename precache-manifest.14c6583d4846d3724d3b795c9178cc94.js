c82d52dbda36d7c0900a7bd527b8097c7cd94623
karmal3 <43403756+karmal3@users.noreply.github.com>
2020-12-29 22:20:53 +0200

    Updates

---
+++ precache-manifest.14c6583d4846d3724d3b795c9178cc94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff3c548aee199c971094dbac729d67f",
+    "url": "/webdeploy/index.html"
+  },
+  {
+    "revision": "a87c78e68a4fb33f9105",
+    "url": "/webdeploy/static/css/2.cfed7de4.chunk.css"
+  },
+  {
+    "revision": "178ff382e82dc4eaa248",
+    "url": "/webdeploy/static/css/main.0747babc.chunk.css"
+  },
+  {
+    "revision": "a87c78e68a4fb33f9105",
+    "url": "/webdeploy/static/js/2.65f14fc2.chunk.js"
+  },
+  {
+    "revision": "2bf8b774c7f3a6a41b6c5b3f18dc18b1",
+    "url": "/webdeploy/static/js/2.65f14fc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178ff382e82dc4eaa248",
+    "url": "/webdeploy/static/js/main.f70434a4.chunk.js"
+  },
+  {
+    "revision": "7e9ebf3da8fbf48912e8",
+    "url": "/webdeploy/static/js/runtime-main.0c0f02e3.js"
+  }
+]);
